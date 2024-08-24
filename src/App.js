ad1da783d86603adefb640472e76ba5823453cb5
Vasavi K <vasavikasireddi@gmail.com>
2024-08-24 23:15:54 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React here
+          Learn React
         </a>
       </header>
     </div>
